0eb666d7208ac1800c316020fcf676ee3c21b9c4
Craig <craigjope@gmail.com>
2021-10-10 09:39:37 -0600

    probably a better way to replace the last element

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length == 2
+    array.join(" and ")
+elsif array.length >2
+    last_one = array[-1]
+    oxford_add = "and #{last_one}"
+    array.pop
+    array << oxford_add
+    array.join(", ")
+else
+array.join
+end
 end
